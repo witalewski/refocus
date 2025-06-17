b1d70cc13b355e808d1f1e0d14bf6252f4cf10ca
Krzysztof Adam Witalewski <witalewski@gmail.com>
2025-06-17 14:32:26 +0200

    change base path to /refocus

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: "export",
+  basePath: "/refocus",
 };
 
 export default nextConfig;
